8314eb44a3aad91a3048911e65f3d78fd46744ed
brncd <brunocarnales@gmail.com>
2023-09-03 21:33:13 -0300

    9-element_length.py

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+"""Values with the appropriate types."""
+
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Values with the appropriate types."""
+    return [(i, len(i)) for i in lst]
