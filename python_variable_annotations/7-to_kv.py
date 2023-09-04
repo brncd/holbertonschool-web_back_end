5fba0ca45427aba4f3465c9f3b2041a46f08e7b9
brncd <brunocarnales@gmail.com>
2023-09-03 21:24:36 -0300

    7-to_kv.py

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+"""Type-annotated function to_kv"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Returns a tuple"""
+    return (k, v**2)
