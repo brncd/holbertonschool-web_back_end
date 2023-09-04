ab21c9556609687a6d5af7931a183c414ea2d71d
brncd <brunocarnales@gmail.com>
2023-09-03 22:32:57 -0300

    1-concurrent_coroutines.py

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -2,7 +2,8 @@
 """Basic Async Syntax"""
 
 
-import asyncio, random
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,19 @@
+"""Let's execute multiple coroutines at the same time with async."""
+
+
+import asyncio
+from typing import List
+from random import uniform
+
+
+async def wait_random(max_delay: int) -> float:
+    """Waits for a random delay between 0 and max_delay seconds."""
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Waits for a random delay between 0 and max_delay seconds."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
