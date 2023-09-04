f34cf3039ab314a9842e0657dbd4b81d4c413d0a
brncd <brunocarnales@gmail.com>
2023-09-03 21:28:23 -0300

    8-make_multiplier.py

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,8 @@
+"""Type-annotated functions."""
+
+
+from typing import Callable
+
+def make_multiplier(multiplier: float) -> callable[[float], float]:
+    """Return a function that multiplies a float by multiplier."""
+    return lambda x: x * multiplier
