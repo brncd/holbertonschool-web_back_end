558056d3b59583fe4536cb58b456220eff726890
brncd <brunocarnales@gmail.com>
2023-09-03 21:57:53 -0300

    shebang

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Variables with the specified values."""
 
 
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """Type-annotated function sum_list."""
 
 from typing import List
