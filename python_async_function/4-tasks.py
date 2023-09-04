ccd966e4548b8c0290b40e766408066fd97c879f
brncd <brunocarnales@gmail.com>
2023-09-03 22:47:55 -0300

    4-tasks.py

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -10,5 +10,8 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Returns a asyncio.Task."""
-    return asyncio.run(wait_n(n, max_delay))
+    """Returns the list of all the delays."""
+    start_time = asyncio.get_event_loop().time()
+    delays = asyncio.run(wait_n(n, max_delay))
+    total_time = asyncio.get_event_loop().time() - start_time
+    return [total_time / n for i in range(n)]
