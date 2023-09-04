5c8ccf89c4f8969ec8b02c6445f8ef88da1f1594
brncd <brunocarnales@gmail.com>
2023-09-03 22:38:02 -0300

    2-measure_runtime.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,6 +13,7 @@ async def wait_random(max_delay: int) -> float:
     await asyncio.sleep(delay)
     return delay
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Waits for a random delay between 0 and max_delay seconds."""
     tasks = [wait_random(max_delay) for _ in range(n)]
