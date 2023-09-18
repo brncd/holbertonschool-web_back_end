448f1d919f87a039dfb6d95607030d2e00eb5428
brncd <brunocarnales@gmail.com>
2023-09-17 21:55:11 -0300

    10-loops.js

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
+
 export default function appendToEachArrayValue(array, appendString) {
     const newArray = [];
     for (const value of array) {
-        newArray.push(`${appendString}${value}`);
+      newArray.push(`${appendString}${value}`);
     }
     return newArray;
-}
+  }
+  
